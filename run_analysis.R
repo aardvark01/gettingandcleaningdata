db927b61a89e22b1ec3126879eaba70879b9db1d
Ken H <ken@woelfware.com>
2014-09-21 13:53:40 -0500

    updated the program

--- run_analysis.R
+++ run_analysis.R
@@ -1,62 +1,108 @@
+#load packages
+install.packages("data.table")
+library("data.table")
+
 run_analysis <- function(){
-        #read in all test files.
-        x.test = read.table("UCI HAR Dataset/test/X_test.txt")
-        y.test = read.table("UCI HAR Dataset/test/y_test.txt")
-        subject.test = read.table("UCI HAR Dataset/test/subject_test.txt")
-        #read in all train files.
-        x.train = read.table("UCI HAR Dataset/train/X_train.txt")
-        y.train = read.table("UCI HAR Dataset/train/y_train.txt")
-        subject.train = read.table("UCI HAR Dataset/train/subject_train.txt")
-        #read in the feature listing file
-        features = read.table("UCI HAR Dataset/features.txt")
-        activities = read.table("UCI HAR Dataset//activity_labels.txt")
-        
-        #merge training and test files
-        combined.x = rbind(x.test, x.train)
-        combined.y = rbind(y.test, y.train)
-        combined.subject = rbind(subject.test, subject.train)
-        
-        #name columns of combined.x dataframe.
-        #setNames(as.data.frame(combined.x),features$V2)
-        names(combined.x) = features$V2
-        
-        #build comparison and remove columns that are not needed.
-        features.mean <- grepl(glob2rx("*mean*"), features$V2)
-        features.std <- grepl(glob2rx("*std*"), features$V2)
-        features.good <- features.mean | features.std
-        combined.x = subset(combined.x, select = features.good)
-        
-        #trim activity strings
-        activity <- sub("_", "", activities[1,2])
-        
-        #replace numbers with descriptions combined.y dataframe
-        y <- sub(activities[1,1],activity,combined.y$V1)
-        for(i in 2:nrow(activities)){
-                activity <- sub("_", "", activities[i,2]) #trim activity strings
-                y <- sub(activities[i,1],activity,y)
+        #check if data directory exists
+        if(!file.exists("UCI HAR Dataset")){
+                temp <- tempfile()
+                fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+                download.file(fileUrl, temp)
+                unzip(temp)
+                rm(temp)
+                rm(fileUrl)
         }
-        combined.y <- data.frame(y)
+        #set the working directory
+        setwd("UCI HAR Dataset/")
         
-        #name remaining columns
-        names(combined.y) = "activity"
-        names(combined.subject) = "subject"
+        #read in the feature listing file
+        features = read.table("features.txt", col.names = c("id","label"))
+
+        #build comparison and remove columns that are not needed.
+        featmean <- grepl(glob2rx("*mean*"), features$label)
+        featstd <- grepl(glob2rx("*std*"), features$label)
+        goodFeatures <- featmean | featstd
+        rm(featmean)
+        rm(featstd)
+        selectFeatures <- features[goodFeatures,]
+        rm(features)
         
         
-        #merge the merged files using rbind
-        tidy_data = cbind(combined.subject,combined.y, combined.x)
+        #read in the activities
+        activities = read.table("activity_labels.txt", col.names = c("id","label"))
+
+        ##################################################
+        #deal with testing data
+        #read in all test files.
+        xtest <- read.table("test/X_test.txt")[,selectFeatures$id]
+        ytest <- read.table("test/y_test.txt")
+        stest <- read.table("test/subject_test.txt")
         
-        write.table(tidy_data, file="tidy.txt")
+        #name the columns
+        names(xtest) <- selectFeatures$label
+        names(ytest) <- "activity"
+        names(stest) <- "subject"
+        
+        #merge test variables
+        test <- cbind(stest, ytest, xtest)
+        
+        #remove unused test variables
+        rm(xtest)
+        rm(ytest)
+        rm(stest)
+        
+        
+        ##################################################
+        #deal with training data
+        #read in all train files.
+        xtrain = read.table("train/X_train.txt")[,selectFeatures$id]
+        ytrain = read.table("train/y_train.txt")
+        strain = read.table("train/subject_train.txt")
+        
+        #name the columns
+        names(xtrain) <- selectFeatures$label
+        names(ytrain) <- "activity"
+        names(strain) <- "subject"
+        
+        #merge test variables
+        train <- cbind(strain, ytrain, xtrain)
+        
+        #remove unused test variables
+        rm(xtrain)
+        rm(ytrain)
+        rm(strain)
+
+        
+        ##################################################
+        #combine training and testing data
+        combined <- rbind(test, train)
+        
+        #remove unused variables
+        rm(test)
+        rm(train)
+        
+        #replace activity numbers with activity names
+        combined$activity <- factor(combined$activity, levels=activities$id, labels = activities$label)
+        
+        rm(activities)
+        rm(selectFeatures)
+        
+        
+        tidy_data <-data.frame(combined)
+        tidy_names <- names(combined)
+        
+        write.table(tidy_data, file="../tidy.txt")
         
         #5.Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-        good_names <- names(tidy_data)
         tidy_averages <- aggregate(tidy_data[,3] ~tidy_data$subject + tidy_data$activity, data=tidy_data, FUN=mean)
         for(i in 4:ncol(tidy_data)){
                 tidy_averages[,i] <- aggregate(tidy_data[,i] ~tidy_data$subject + tidy_data$activity, data=tidy_data, FUN=mean)
         }
         
-        names(tidy_averages) <- good_names
+        names(tidy_averages) <- tidy_names
         
         write.table(tidy_averages, file="tidy_averages.txt")
+        }
 }
 
 
